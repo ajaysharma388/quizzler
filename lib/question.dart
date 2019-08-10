f30fe796b179d463ec863e4cb74cfaf914cab49c
AJAY SHARMA <ajaysharma2201@gmail.com>
2019-08-10 21:37:59 +0530

    update.2019.08.10.3

--- lib/question.dart
+++ lib/question.dart
@@ -1,8 +1,5 @@
 class Question {
   String QuestionStatement;
   bool QuestionAnswer;
-  Question({String q, bool a}) {
-    QuestionAnswer = a;
-    QuestionStatement = q;
-  }
+  Question({this.QuestionStatement, this.QuestionAnswer});
 }
--- lib/questionbank.dart
+++ lib/questionbank.dart
@@ -3,33 +3,51 @@ import './question.dart';
 class QuestionBank {
   int _currentQuestion = 0;
   List<Question> _question = [
-    Question(q: 'Some cats are actually allergic to humans', a: true),
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-    Question(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
-    Question(q: 'It is illegal to pee in the Ocean in Portugal.', a: true),
+        QuestionStatement: 'Some cats are actually allergic to humans',
+        QuestionAnswer: true),
     Question(
-        q: 'No piece of square dry paper can be folded in half more than 7 times.',
-        a: false),
+        QuestionStatement: 'You can lead a cow down stairs but not up stairs.',
+        QuestionAnswer: false),
     Question(
-        q: 'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        a: true),
+        QuestionStatement:
+            'Approximately one quarter of human bones are in the feet.',
+        QuestionAnswer: true),
     Question(
-        q: 'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        a: false),
+        QuestionStatement: 'A slug\'s blood is green.', QuestionAnswer: true),
     Question(
-        q: 'The total surface area of two human lungs is approximately 70 square metres.',
-        a: true),
-    Question(q: 'Google was originally called \"Backrub\".', a: true),
+        QuestionStatement: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+        QuestionAnswer: true),
     Question(
-        q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        a: true),
+        QuestionStatement: 'It is illegal to pee in the Ocean in Portugal.',
+        QuestionAnswer: true),
     Question(
-        q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        a: true),
+        QuestionStatement:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        QuestionAnswer: false),
+    Question(
+        QuestionStatement:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        QuestionAnswer: true),
+    Question(
+        QuestionStatement:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        QuestionAnswer: false),
+    Question(
+        QuestionStatement:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        QuestionAnswer: true),
+    Question(
+        QuestionStatement: 'Google was originally called \"Backrub\".',
+        QuestionAnswer: true),
+    Question(
+        QuestionStatement:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        QuestionAnswer: true),
+    Question(
+        QuestionStatement:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        QuestionAnswer: true),
   ];
   void nextQuestion() {
     if (_currentQuestion < _question.length) {
